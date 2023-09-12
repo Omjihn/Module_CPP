fc0f94e301f7e3137994d628f1dd7d1e54200f70
Gregoire Bricot <gbricot@c1r1p4.42perpignan.fr>
2023-09-12 17:51:42 +0200

    ex 00 form cpp00 added

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbricot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 17:07:10 by gbricot           #+#    #+#             */
+/*   Updated: 2023/09/12 17:44:58 by gbricot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	tab = 1;
+
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	while (av[tab])
+	{
+		i = 0;
+		while (av[tab][i])
+		{
+			if (av[tab][i] >= 'a' && av[tab][i] <= 'z')
+				av[tab][i] -= 32;
+			i++;
+		}
+		tab++;
+	}
+	tab = 1;
+	while (av[tab])
+		std::cout << av[tab++];
+	std::cout << "\n";
+	return (0);
+}
