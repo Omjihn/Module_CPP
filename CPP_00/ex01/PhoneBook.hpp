9ef5309a978a30ae570011c6aea6f5441f4b661f
Gregoire Bricot <gbricot@c1r1p3.42perpignan.fr>
2023-09-16 18:40:11 +0200

    Shit I deleted all my cpp files :x

---
+++ CPP_00/ex01/PhoneBook.hpp
@@ -0,0 +1,13 @@
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class	PhoneBook
+{
+public:
+
+	PhoneBook( void );
+	~PhoneBook( void );
+};
+
+#endif
---
+++ CPP_00/ex01/phonebook.hpp
@@ -0,0 +1,9 @@
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <string.h>
+# include "PhoneBook.hpp"
+
+#endif
