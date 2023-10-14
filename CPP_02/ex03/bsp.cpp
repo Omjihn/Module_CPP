af4fe4e7f22a97435d33a4c21acb54db82547012
Gregoire Bricot <gbricot@c1r2p2.42perpignan.fr>
2023-10-14 20:45:56 +0200

    some changes

--- CPP_02/ex03/bsp.cpp
+++ CPP_02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 18:58:07 by gbricot           #+#    #+#             */
-/*   Updated: 2023/10/14 20:41:06 by gbricot          ###   ########.fr       */
+/*   Updated: 2023/10/14 20:44:52 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ static Fixed   ft_get_side( Point const &v_start, Point const &v_end, Point cons
 
 bool    bsp( Point const &a, Point const &b, Point const &c, Point const &p )
 {
-    std::cout << "\033[0;37mTriangle coords :" << std::endl << std::endl;
+    std::cout << std::endl << "\033[0;37mTriangle coords :" << std::endl << std::endl;
     std::cout << "a.x = " << a.get_fix_x() << " | a.y = " << a.get_fix_y() << std::endl;
     std::cout << "b.x = " << b.get_fix_x() << " | b.y = " << b.get_fix_y() << std::endl;
     std::cout << "c.x = " << c.get_fix_x() << " | c.y = " << c.get_fix_y() << std::endl << std::endl;
-    std::cout << "Is that point is inside the triangle ?" << std::endl << std::endl;
+    std::cout << "Is the point inside the triangle ?" << std::endl << std::endl;
     std::cout << "p.x = " << p.get_fix_x() << " | p.y = " << p.get_fix_y() << std::endl << std::endl;
 
     Fixed res_1(ft_get_side(a, b, p));
--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 18:54:56 by gbricot           #+#    #+#             */
-/*   Updated: 2023/10/14 20:32:47 by gbricot          ###   ########.fr       */
+/*   Updated: 2023/10/14 20:42:31 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,7 @@ int main(void)
     ft_test_case(Point(0, 0), Point(10, 30), Point(20, 0), Point(30, 15));
     
     /*              Create yours own tests                           */
-    ft_test_case(Point(0, 0), Point(50, 150), Point(200, -70), Point(100, -12));
+    //ft_test_case(Point(0, 0), Point(50, 150), Point(200, -70), Point(100, -12));
 
-    //std::cout << std::endl << "More details : " << "https://planetcalc.com/8108/" << std::endl;
     return (0);
 }
