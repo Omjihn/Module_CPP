f8198fa148853b508aac1b07072b4c2823f6feb5
Gregoire Bricot <gbricot@c1r1p1.42perpignan.fr>
2023-10-16 13:56:31 +0200

    lil update in Clap trap .cpp

--- CPP_03/ex00/ClapTrap.cpp
+++ CPP_03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 18:41:59 by gbricot           #+#    #+#             */
-/*   Updated: 2023/10/15 20:05:06 by gbricot          ###   ########.fr       */
+/*   Updated: 2023/10/16 13:54:53 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,34 +35,46 @@ ClapTrap::~ClapTrap( void )
 
 void    ClapTrap::attack( const std::string &target )
 {
-    if (energy_point <= 0)
-    {
-        std::cout << "ClapTrap " << this->name << " has no energy left it can't do anything :(" << std::endl;
-        return ;
-    }
-    else if (hit_point <= 0)
+    if (hit_point == 0)
     {
         std::cout << "ClapTrap " << this->name << " is knock-out it can't do anthing :(" << std::endl;
         return ;
     }
+    else if (energy_point == 0)
+    {
+        std::cout << "ClapTrap " << this->name << " has no energy left it can't do anything :(" << std::endl;
+        return ;
+    }
     this->energy_point--;
-    std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->attack_dmg << " points of damage !" << std::endl;
+    std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->attack_dmg << " HP !" << std::endl;
 }
 
 void    ClapTrap::takeDamage( unsigned int amount )
 {
-    if (hit_point <= 0)
+    if (hit_point == 0)
     {
-        std::cout << "ClapTrap " << this->name << " is already dying on the ground but it's getting attacked hard life :/" << std::endl;
+        std::cout << "ClapTrap " << this->name << " is already dying on the ground but it's getting attacked, hard life :/" << std::endl;
         return ;
     }
-    this->hit_point -= amount;
+    if (this->hit_point >= amount)
+        this->hit_point -= amount;
+    else
+        this->hit_point = 0;
     std::cout << "ClapTrap " << this->name << " is getting attacked and loose " << amount << " HP." << std::endl;
+    if (hit_point == 0)
+        std::cout << "ClapTrap " << this->name << " is now knocked-out." << std::endl;
+    else
+        std::cout << "ClapTrap " << this->name << " has " << this->hit_point << " HP left." << std::endl;
 }
 
 void    ClapTrap::beRepaired( unsigned int amount )
 {
-    if (energy_point <= 0)
+    if (hit_point == 0)
+    {
+        std::cout << "ClapTrap " << this->name << " is knock-out it can't do anthing :(" << std::endl;
+        return ;
+    }
+    else if (energy_point == 0)
     {
         std::cout << "ClapTrap " << this->name << " can't repair itself.. No more energy left :(" << std::endl;
         return ;
--- CPP_03/ex00/main.cpp
+++ CPP_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 18:41:26 by gbricot           #+#    #+#             */
-/*   Updated: 2023/10/15 20:05:51 by gbricot          ###   ########.fr       */
+/*   Updated: 2023/10/16 11:11:45 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int main(void)
     ClapTrap    test("Etalon du cul");
 
     test.attack("Brick");
+    test.takeDamage(5);
     test.beRepaired(10);
-    
+    test.takeDamage(15);
+    test.attack("Brick");
+    test.takeDamage(15);
+    test.beRepaired(10);
+    return (0);
 }
