d5951f79f8218772cc5defa2453a742915dd65f0
Gregoire Bricot <gbricot@c1r1p2.42perpignan.fr>
2023-11-23 15:25:19 +0100

    Form class created need to understand why it dosent find the Bureaucrat class, probably a wrong include

---
+++ CPP_05/ex01/Form.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 13:52:58 by gbricot           #+#    #+#             */
+/*   Updated: 2023/11/23 15:13:24 by gbricot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form( void ) : name("Interesting form"), grade_s(75), grade_e(75)
+{
+    this->is_signed = 0;
+    std::cout << "[Form] Default constructor called" << std::endl;
+}
+
+Form::Form( const std::string input_name, const int i_grade_s, const int i_grade_e )  : name(input_name), grade_s(i_grade_s), grade_e(i_grade_e)
+{
+    if (this->grade_e > 150 || this->grade_s > 150)
+        throw   Form::GradeTooLowException();
+    else if (this->grade_e < 1 || this->grade_s < 1)
+        throw   Form::GradeTooHighException();
+    this->is_signed = 0;
+    std::cout << "[Form] String/Grades constructor called" << std::endl;
+}
+
+Form::Form( Form &val ) : name(val.getName()), grade_s(val.getGrade_s()), grade_e(val.getGrade_e())
+{
+    this->is_signed = val.ft_is_signed();
+    std::cout << "[Form] Copy constructor called" << std::endl;
+}
+
+Form::~Form()
+{
+    std::cout << "[Form] Destructor called" << std::endl;
+}
+
+Form & Form::operator=( Form &val )
+{
+    this->is_signed = val.ft_is_signed();
+    return (*this);
+}
+
+std::ostream    & operator<<( std::ostream &os, Form& val )
+{
+    os << "The form : " << val.getName() << " needs " << val.getGrade_s() << \
+        " to be signed and " << val.getGrade_e() << " to be executed." << "The form is " <<  std::endl;
+    if (!val.ft_is_signed())
+        os << "not ";
+    os << "signed." << std::endl;
+    return (os);
+}
+
+void    Form::beSigned( Bureaucrat &signer )
+{
+    if (this->is_signed)
+        return ;
+    if (signer.getGrade() < this->getGrade_s())
+        throw   Form::GradeTooLowException();
+    this->is_signed = 1;
+}
+
+const std::string   Form::getName( void )
+{
+    return (this->name);
+}
+
+int   Form::getGrade_s( void )
+{
+    return (this->grade_s);
+}
+
+int   Form::getGrade_e( void )
+{
+    return (this->grade_e);
+}
+
+bool    Form::ft_is_signed( void )
+{
+    return (this->is_signed);
+}
---
+++ CPP_05/ex01/Form.hpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 13:52:40 by gbricot           #+#    #+#             */
+/*   Updated: 2023/11/23 15:16:52 by gbricot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+
+# include <iostream>
+# include <ostream>
+# include <exception>
+# include "Bureaucrat.hpp"
+
+class   Form
+{
+    public:
+
+        /*      CONSTRUCTORS/DESTRUCTOR    */
+
+        Form( void );
+        Form( const std::string input_name, const int i_grade_s, const int i_grade_e );
+        Form( Form &val );
+        ~Form();
+
+        /*      EXCEPTIONS CLASSES      */
+
+        class   GradeTooHighException : public std::exception
+        {
+            public:
+
+                virtual const char* what( void ) const throw();
+        };
+
+        class   GradeTooLowException : public std::exception
+        {
+            public:
+
+                virtual const char* what( void ) const throw();
+        };
+
+        /*      MEMBERS FUNCTIONS       */
+
+        Form                & operator=( Form &val );
+        friend std::ostream & operator<<( std::ostream& os, Form& val);
+
+        void    beSigned( Bureaucrat &signer );
+
+        const std::string   getName( void );
+        int           getGrade_s( void );
+        int           getGrade_e( void );
+        bool                ft_is_signed( void );
+
+    private:
+
+        const std::string   name;
+        const int           grade_s;
+        const int           grade_e;
+        bool                is_signed;
+
+};
+
+#endif
