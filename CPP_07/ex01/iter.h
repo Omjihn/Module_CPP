e2d3f95a11ec2cd4cab50c2502a58894a1c98549
Omjihn <le.greg09100@gmail.com>
2024-02-13 10:14:13 +0100

    ex01 updated mybe need to change the main

--- CPP_07/ex01/iter.h
+++ CPP_07/ex01/iter.h
@@ -6,7 +6,7 @@
 /*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 05:00:29 by gbricot           #+#    #+#             */
-/*   Updated: 2024/02/13 05:20:28 by gbricot          ###   ########.fr       */
+/*   Updated: 2024/02/13 09:37:15 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,14 @@ void	remplace_even_odd( T &i )
 		i = 1;
 }
 
-template< typename A, typename L, typename F >
-void	iter( A *array, L &lenght, F &func )
+template< typename T >
+void	print_val( T &i )
+{
+	std::cout << '\t' << i << std::endl;
+}
+
+template< typename A, typename L >
+void	iter( A *array, L &lenght, void (*func)(A &val) )
 {
 	if (!array || !func)
 		return ;
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbricot <gbricot@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 05:00:09 by gbricot           #+#    #+#             */
-/*   Updated: 2024/02/13 05:19:21 by gbricot          ###   ########.fr       */
+/*   Updated: 2024/02/13 09:27:25 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 
 int	main(void)
 {
+
+	/*		1ST TEST		*/
+
+	std::cout << "1st test :" << std::endl;
 	std::srand(getpid());
 	int arr_len = 5;
 	int arr[arr_len] = {	std::rand(),
@@ -25,10 +29,27 @@ int	main(void)
 							std::rand() };
 
 	std::cout << "arr = " << std::endl;
-	for (int i = 0; i < 5; i++)
-		std::cout << '\t' << arr[i] << std::endl;
+	for (int i = 0; i < arr_len; i++)
+		print_val(arr[i]);
 	iter(arr, arr_len, remplace_even_odd);
 	std::cout << "arr = " << std::endl;
-	for (int i = 0; i < 5; i++)
-		std::cout << '\t' << arr[i] << std::endl;
+	for (int i = 0; i < arr_len; i++)
+		print_val(arr[i]);
+
+	/*		2ND TEST		*/
+
+	std::cout << "2nd test :" << std::endl;
+	char	arr1[arr_len] = {	'S',
+								'a',
+								'l',
+								'u',
+								't' };
+	std::cout << "arr = " << std::endl;
+	for (int i = 0; i < arr_len; i++)
+		print_val(arr1[i]);
+	iter(arr1, arr_len, remplace_even_odd);
+	std::cout << "arr = " << std::endl;
+	for (int i = 0; i < arr_len; i++)
+		print_val(arr1[i]);
+	return (0);
 }
